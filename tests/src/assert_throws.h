559d618757b21ea5f7ea6293454473d98f3ddacf
to-miz <tolga.mizrak@googlemail.com>
2018-10-06 00:25:19 +0200

    added common merge files

--- tests/src/assert_throws.h
+++ tests/src/assert_throws.h
@@ -6,4 +6,8 @@
 void enable_asserts(bool enabled);
 void assert_throws(bool cond, const char* file, int line);
 
-#endif // _ASSERT_THROWS_H_INCLUDED_
+#define TM_ASSERT(x) assert_throws(x, __FILE__, __LINE__)
+#define CHECK_ASSERTION_FAILURE(x) CHECK_THROWS(x)
+#define CHECK_NOASSERT(x) CHECK_NOTHROW(x)
+
+#endif  // _ASSERT_THROWS_H_INCLUDED_
