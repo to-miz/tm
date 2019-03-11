ead846af4601ec9e705f0fb6f15744e95038163d
to-miz <tolga.mizrak@googlemail.com>
2019-03-11 09:07:33 +0100

    Improved testing assertions.

--- tests/src/assert_throws.cpp
+++ tests/src/assert_throws.cpp
@@ -1,7 +1,8 @@
 static bool asserts_enabled = true;
 void enable_asserts(bool enabled) { asserts_enabled = enabled; }
-void assert_throws(bool cond, const char* file, int line) {
+void assert_throws(bool cond, const char* cond_string, const char* file, int line) {
     if (!cond && asserts_enabled) {
-        throw std::runtime_error(std::string("testing assertion throw: ") + file + ":" + std::to_string(line));
+        throw std::runtime_error(std::string("Testing assertion throw: (") + cond_string + ") " + file + ":" +
+                                 std::to_string(line));
     }
 }
--- tests/src/assert_throws.h
+++ tests/src/assert_throws.h
@@ -6,8 +6,8 @@
 void enable_asserts(bool enabled);
 void assert_throws(bool cond, const char* file, int line);
 
-#define TM_ASSERT(x) assert_throws(x, __FILE__, __LINE__)
-#define CHECK_ASSERTION_FAILURE(x) CHECK_THROWS(x)
-#define CHECK_NOASSERT(x) CHECK_NOTHROW(x)
+#define TM_ASSERT(x) assert_throws(x, #x, __FILE__, __LINE__)
+#define CHECK_ASSERTION_FAILURE CHECK_THROWS
+#define CHECK_NOASSERT CHECK_NOTHROW
 
 #endif  // _ASSERT_THROWS_H_INCLUDED_
